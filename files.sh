4d1cba24813e8386e3bc175190d13ba4cb61e0a3
Appbank4 <118092670+Appbank4@users.noreply.github.com>
2023-01-20 22:38:20 -0500

    Update files.sh

--- files.sh
+++ files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-echo "please Enter the directoryname: "
-read directoryname
 echo -n "Please Enter the filename: "
 read filename
 today=`date +%m-%d-%y`
 user=`whoami`
-touch $directory.$filename.$user.$today
+touch $filename.$user.$today
 echo "Thank you!"
-echo "Welcome to Etech Consulting" >> $directory.$filename.$user.$today
-df -h $directory.$filename.$user.$today
+echo "Welcome to Etech Consulting" >> $filename.$user.$today
+df -h $filename.$user.$today
